c36663174d977daf8f97ea98c5b30a4c703b31ab
MichihideS <michihides@gmail.com>
2024-12-01 02:12:25 +0100

    initial commit

---
+++ src/main/kotlin/com/michihides/laboration_01/Laboration01Application.kt
@@ -0,0 +1,11 @@
+package com.michihides.laboration_01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Laboration01Application
+
+fun main(args: Array<String>) {
+	runApplication<Laboration01Application>(*args)
+}
