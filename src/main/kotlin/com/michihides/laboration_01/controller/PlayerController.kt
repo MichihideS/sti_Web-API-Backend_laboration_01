1f796a5af41e89547294b8980400a4512c0affa5
MichihideS <michihides@gmail.com>
2024-12-02 13:49:02 +0100

    add post function to create new player

--- src/main/kotlin/com/michihides/laboration_01/controller/PlayerController.kt
+++ src/main/kotlin/com/michihides/laboration_01/controller/PlayerController.kt
@@ -1,7 +1,19 @@
 package com.michihides.laboration_01.controller
 
 import com.michihides.laboration_01.model.Player
+import com.michihides.laboration_01.repository.PlayerRepository
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
-class PlayerController {
+@RestController
+class PlayerController(
+    private val playerRepository: PlayerRepository
+) {
+    @PostMapping("/player")
+    fun postNewPlayer(@RequestBody player: Player): Player {
+
+        return playerRepository.save(player)
+    }
 
 }
