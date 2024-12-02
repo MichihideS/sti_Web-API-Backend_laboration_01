754f7ba08a1d64816b234b4a71cbe4085a131779
MichihideS <michihides@gmail.com>
2024-12-02 16:11:31 +0100

    add delete function by user id

--- src/main/kotlin/com/michihides/laboration_01/controller/PlayerController.kt
+++ src/main/kotlin/com/michihides/laboration_01/controller/PlayerController.kt
@@ -3,12 +3,14 @@ package com.michihides.laboration_01.controller
 import com.michihides.laboration_01.model.Player
 import com.michihides.laboration_01.repository.PlayerRepository
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/player")
@@ -38,4 +40,23 @@ class PlayerController(
         return ResponseEntity.status(201).body("Player Created")
     }
 
+    // PUT
+
+    // DELETE
+    @DeleteMapping
+    fun deletePlayerByPlayerId(@RequestParam("id") id: Long): ResponseEntity<Long> {
+        val foundPlayerOptional = playerRepository.findById(id)
+
+        if (foundPlayerOptional.isPresent) {
+            val playerId = foundPlayerOptional.get().id
+
+            if (playerId != null) {
+                playerRepository.deleteById(playerId)
+
+                return ResponseEntity.status(204).build()
+            }
+        }
+
+        return ResponseEntity.notFound().build()
+    }
 }
